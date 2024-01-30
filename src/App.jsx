e1c248e1e48bc46354cce2db1879a35491cf5fcc
Benedict Adrian <116951749+rianrich123@users.noreply.github.com>
2024-01-30 21:10:36 +0700

    delete color picker from shopify

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ import { Route, Routes } from "react-router-dom";
 // import { Keyboards } from "./components/Keyboards";
 import { Product } from "./components/Product";
 import { Footer } from "./components/Footer";
-import { ColorPickerExample } from "./components/ColorPickerExample";
 
 export default function App() {
   return (
     <>
       <Navigation />
-      <ColorPickerExample />
       <div className="min-h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
